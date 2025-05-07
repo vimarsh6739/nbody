9ca0da02919a010cd102af4ba2bfb21a248e35ad
mtaylo12 <mayat4@illinois.edu>
2025-05-07 17:08:53 -0500

    more refactoring!

--- common/main.h
+++ common/main.h
@@ -12,6 +12,8 @@ extern float MAC_PARAM;
 extern int SHIFT_DIGITS;
 extern int MAX_KEY_LENGTH;
 
+extern bool CUDA;
+
 void randomizeBodies(PhiloxEngine &rng, Body *bodies, int n);
 void createOctree(std::vector<DFTNode> &dft, Octree *octree, Body *bodies,
                   int nBodies);
@@ -19,3 +21,8 @@ void nbodyIterate(Body *p, float dt, int nBodies, int nIters);
 float checkAccuracy(Body *p, Body *orig, int nBodies, int nIters);
 void integratePositions(Body *p, float dt, int start, int end);
 int bodyForce(Body *p, float dt, int n, std::vector<DFTNode> dft);
+void allInteractionsDS(Body *bodies, float dt, int nBodies);
+int MACInteractionsDFT(Body *bodies, float dt, int nBodies,
+                       std::vector<DFTNode> &dft);
+int allInteractionsDFT(Body *bodies, float dt, int nBodies,
+                       std::vector<DFTNode> &dft);
